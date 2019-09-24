78752e24322b8caa226ed4d861571600612cccd2
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-09-24 10:43:11 -0500

    added updates to auth with addition to configs, migrations, and data

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,51 +1,57 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-const Users = require('../user/usermodel.js');
-const secrets = require('../config/secrets.js'); //<<<<<<<
+const Users = require('./auth-model')
+const bcrypt = require('bcryptjs')
+
+const secrets = require('../config/secrets')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
+  let user = req.body
+  
+  if (!user.username || !user.password) {
+    res.status(401).json({Message: "Please provide both a username and a password"})
+  } else {
+    const hash = bcrypt.hashSync(user.password, 12)
+    user.password = hash
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    Users.add(user)
+      .then(user => {
+        res.status(201).json(req.body)
+      })
+      .catch(error => {
+        res.status(500).json({Error: 'there was an issue createing the user account in the database'})
+      })
+  }
 });
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+  let {username, password} = req.body
 
-  Users.findBy({ username })
+  Users.findBy({username})
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
-        res.status(200).json({ token });
+        const token = generateToken(user)
+        res.status(200).json({token})
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({Message: "You have either entered the wrong username or password"})
       }
     })
     .catch(error => {
-      res.status(500).json(error);
-    });
+      res.status(500).json({Error: 'there was an issue logging into the account!'})
+    })
 });
 
+//to create the token
 function generateToken(user) {
   const payload = {
-    username: user.username,
-  };
+    username: user.username
+  }
   const options = {
-    expiresIn: '1d',
-  };
-  // bring in the secret from the secrets file
-  return jwt.sign(payload, secrets.jwtSecret, options);
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options)
 }
 
 module.exports = router;
