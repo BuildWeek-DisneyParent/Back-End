f71db9f7acee6b1c4590f0722e9eabace8b62d4a
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-09-25 22:28:33 -0500

    making heroku work again

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/auth', authRouter);
-server.use('/api/parents', parentRouter);
+//server.use('/api/parents', parentRouter);
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
